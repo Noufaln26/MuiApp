53d59759b328cea094bde3e0ed060224b3c8c922
Noufal N <noufaln26@gmail.com>
2022-04-15 08:39:26 +0530

    Added Mui Navbar and filters option

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import NavBar from './components/AppBar/NavBar';
 import Card from './components/cardview/Card';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
     <Card/>
     
     </div>
--- src/components/cardview/CardView.js
+++ src/components/cardview/CardView.js
@@ -8,9 +8,6 @@ import { CardActionArea } from '@mui/material';
 
 export default function CardView() {
 
-  const randomCOlor=()=>{
-    
-  }
   
    
     
